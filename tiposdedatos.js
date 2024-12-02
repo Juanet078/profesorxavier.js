e0f8c8fabcc10d998039e674f215f6c427704790
unknown <juanruizabad.jra@gmail.com>
2024-12-02 04:22:10 +0100

    primercommit

---
+++ tiposdedatos.js
@@ -0,0 +1,30 @@
+// string
+let nameUser = "xavier";
+
+//number
+//float or int
+nameUser = 777;
+nameUser = 1.1;
+
+//boolean
+nameUser = true;
+
+//undefined
+nameUser = undefined;
+
+// null
+nameUser = null;
+
+//object:
+const person = { firstName: "John", lastName: "Doe" };
+console.log(person.firstName);
+
+// Array object:
+// pos 0, 1, 2
+const cars = ["Saab", "Volvo", "BMW", 777, true, { nota1: 10, nota2: 9 }];
+const car = cars[2];
+
+// Date object:
+const date = new Date("2021-03-25");
+console.log(date.getDay());
+console.log(date.getFullYear());
